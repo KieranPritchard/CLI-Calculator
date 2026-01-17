89766c12fcec7642946c5afb9a2c59d528fc1ed1
Kieran Pritchard <KieranPritchard06@gmail.com>
2026-01-17 20:56:04 +0000

    added add function

--- cli_calculator.go
+++ cli_calculator.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"flag"
+	"strings"
 	"fmt"
+	"strconv"
 )
 
 func main(){
@@ -11,4 +13,29 @@ func main(){
 	sub := flag.String("sub", "", "The numbers you want to subtract ")
 	multi := flag.String("multi", "", "The numbers you want to multiply")
 	div := flag.String("div", "", "The numbers you want to multiply")
+
+	// Parses the flags
+	flag.Parse()
+
+	// Checks for the add flag
+	if *add != "" {
+		// Splits the numbers
+		numbers := strings.SplitAfter(*add, " ")
+
+		// Converts the numbers in the arguement to integers
+		num_1, err := strconv.Atoi(numbers[0])
+		num_2, err := strconv.Atoi(numbers[1])
+
+		// Checks if the error is not nil
+		if err != nil{
+			// Outputs the error message
+			fmt.Println("Error", err)
+		} else {
+			// Calculates the sum
+			sum := num_1 + num_2
+			
+			// Outputs the sum
+			fmt.Println(sum)
+		}
+	}
 }
