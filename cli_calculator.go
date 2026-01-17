ba0826cb00943d36f80f1317666edd1e8ba40d5e
Kieran Pritchard <KieranPritchard06@gmail.com>
2026-01-17 20:38:16 +0000

    added flags to the code

--- cli_calculator.go
+++ cli_calculator.go
@@ -4,3 +4,11 @@ import (
 	"flag"
 	"fmt"
 )
+
+func main(){
+	// Defines command line flags i will be using
+	add := flag.String("add","", "The numbers you want to add together")
+	sub := flag.String("sub", "", "The numbers you want to subtract ")
+	multi := flag.String("multi", "", "The numbers you want to multiply")
+	div := flag.String("div", "", "The numbers you want to multiply")
+}
